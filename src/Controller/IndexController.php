8362a28641cb31a6cf242b889f659854c5072d4d
Alex Robert <contact.alexrobert04@gmail.com>
2021-09-06 16:23:22 +0200

    Add StartPage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('index/index.html.twig');
+    }
+
+    #[Route('/start')]
+    public function start(): Response
+    {
+        return $this->render('index/start.html.twig');
     }
 }
